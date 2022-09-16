f1ac16e3103a8272000ed361d7046bf782d28ddb
Abhay Aysola <abhay.ayyasola@gmail.com>
2022-09-16 12:56:44 +0530

    feat(protobuf + keyexchange.proto)

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
-DEPS := libcurl json-c
+DEPS := libcurl json-c 'libprotobuf-c >= 1.0.0'
 
 all: build/main
 
+.PHONY: run
+run: clean proto build/main
+	clear
+	@build/main
+
 build/main: build/main.o build/ap.o
-	gcc $? $$(pkg-config --libs --cflags $(DEPS)) -o build/main
+	gcc $? protocol/*.pb-c.c -I include $$(pkg-config --libs --cflags $(DEPS)) -o build/main
 
 build/main.o : src/main.c
 	gcc $? -I include -o $@ -c
@@ -11,7 +16,20 @@ build/main.o : src/main.c
 build/ap.o : src/ap.c
 	gcc $? -I include -o $@ -c
 
+.PHONY: proto
+proto:
+	@protoc --c_out=. protocol/*
+	@mv protocol/*pb-c.h include/
+	@sed -i -e 's/protocol\///' protocol/keyexchange.pb-c.c
+	@echo "Proto files generated"
+
 .PHONY: clean
 clean:
 	@rm build/* -f
+	@rm protocol/*pb-c.c -f
+	@rm include/*pb-c.h -f
 	@echo "Cleaned"
+
+.PHONY: format
+format:
+	@astyle --options="astyle-code-format.cfg" "src/*.c,*.h" "include/*.c,*.h"
--- src/main.c
+++ src/main.c
@@ -1,43 +1,52 @@
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <sys/types.h>
+// #include <sys/socket.h>
+// #include <netdb.h>
+// #include <string.h>
+// #include <arpa/inet.h>
+// #include "ap.h"
+
+// int
+// main(void) {
+//     // Initialize socket data
+//     struct addrinfo hints, *res;
+//     int sockfd;
+//     memset(&hints, 0, sizeof hints);
+//     hints.ai_family = AF_INET;
+//     hints.ai_socktype = SOCK_STREAM;
+
+//     char* ap = get_ap(NULL, 0);
+
+//     // Split ap into port and ap on ':' character
+//     char* port = malloc(5);
+//     for (int i = 0; ap[i - 1] != ':'; i++) {
+//         port = &ap[i + 1];
+//     }
+
+//     *(port - 1) = '\0';
+//     printf("ap: %s, port: %s\n", ap, port);
+
+//     int err;
+//     if (err = getaddrinfo(ap, port, &hints, &res)) {
+//         printf("%s", gai_strerror(err));
+//     }
+
+//     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+
+//     char ip_addr[INET_ADDRSTRLEN];
+//     inet_ntop(res->ai_addr->sa_family, res->ai_addr, ip_addr, INET_ADDRSTRLEN);
+//     // printf("%i", connect(sockfd, res->ai_addr, res->ai_addrlen));
+//     printf("%s\n", ip_addr);
+
+//     freeaddrinfo(res);
+// }
+
+
+#include "keyexchange.pb-c.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include "ap.h"
 
-int
-main(void) {
-    // Initialize socket data
-    struct addrinfo hints, *res;
-    int sockfd;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-
-    char* ap = get_ap(NULL, 0);
-
-    // Split ap into port and ap on ':' character
-    char* port = malloc(5);
-    for (int i = 0; ap[i - 1] != ':'; i++) {
-        port = &ap[i + 1];
-    }
-
-    *(port - 1) = '\0';
-    printf("ap: %s, port: %s\n", ap, port);
-
-    int err;
-    if (err = getaddrinfo(ap, port, &hints, &res)) {
-        printf("%s", gai_strerror(err));
-    }
-
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-
-    char ip_addr[INET_ADDRSTRLEN];
-    inet_ntop(res->ai_addr->sa_family, res->ai_addr, ip_addr, INET_ADDRSTRLEN);
-    // printf("%i", connect(sockfd, res->ai_addr, res->ai_addrlen));
-    printf("%s\n", ip_addr);
-
-    freeaddrinfo(res);
+int main(void) {
+    ClientHello test;
+    return 0;
 }
